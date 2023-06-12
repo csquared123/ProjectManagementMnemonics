88d05984df12809f7820d6e4b0c83848fac84ab3
cmcntsh <32034299+cmcntsh@users.noreply.github.com>
2023-06-11 21:45:37 -0600

    Add files via upload

---
+++ js/fundamentalsQuiz3.js
@@ -0,0 +1,186 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: "601m4p71a",
+    question: "What work should be done before project initiation?",
+    answer: "business case for the project",
+    options: [
+      "PMI doesn't outline work before project initation.",
+      "business case for the project",
+      "determine project budget",
+      "assign a project team"
+    ],
+    note: "Source: PROEA 601 module 4 slide 71."
+  },
+    {
+    numb: "601m4p71b",
+    question: "What should be included in a business case?",
+    answer: "All of the above.",
+    options: [
+      "expected benefit",
+      "written or verbal value proposition",
+      "document that includes problem statement, recommended solution, analysis, feasibility, and alternatives",
+      "All of the above."
+    ],
+    note: "Source: PROEA 601 module 4 slide 71."
+  },
+    {
+    numb: "601m5p78",
+    question: "What is the main purpose of the Initiation process group?",
+    answer: "define a new project or phase",
+    options: [
+      "determine a suitable timeline",
+      "determine the budget",
+      "define a new project or phase",
+      "complete the work"
+    ],
+    note: "Source: PROEA 601 module 5 slide 78."
+  },
+    {
+    numb: "601m5p2a",
+    question: "Which project management knowledge areas are part of the Initiating phase?",
+    answer: "Integration, Stakeholder",
+    options: [
+      "Integration",
+      "Integration, Stakeholder",
+      "Integration, Quality, Resource, Communications, Risk, Procurement, Stakeholder",
+      "Integration, Scope, Schedule, Cost, Quality, Resource, Communications, Risk, Procurement, Stakeholder"
+    ],
+    note: "Source: PROEA 601 module 5 slide 2."
+  },
+  {
+    numb: "601m5p2b",
+    question: "Which processes are part of the Initiating phase?",
+    answer: "project charter, stakeholder register",
+    options: [
+      "collect requirements, estsimate costs",
+      "sequence activities, identify risks",
+      "project charter, stakeholder register",
+      "develop management plan, plan stakeholder engagement"
+    ],
+    note: "Source: PROEA 601 module 5 slide 2."
+  },
+  {
+    numb: "601m5p6a",
+    question: "Identify the correct process: written document including high-level project information",
+    answer: "project charter",
+    options: [
+      "project charter",
+      "stakeholder registry",
+      "risk registry",
+      "work breakdown structure (WBS)"
+    ],
+    note: "Source: PROEA 601 module 5 slide 6."
+  },
+  {
+    numb: "601m5p6b",
+    question: "Identify the correct process: at-a-glance guide to why a project is taking place",
+    answer: "project charter",
+    options: [
+      "project charter",
+      "stakeholder registry",
+      "risk registry",
+      "work breakdown structure (WBS)"
+    ],
+    note: "Source: PROEA 601 module 5 slide 6."
+  },
+  {
+    numb: "601m5p6c",
+    question: "Identify the correct process: authorizes a project, but is not a binding contract",
+    answer: "project charter",
+    options: [
+      "project charter",
+      "stakeholder registry",
+      "risk registry",
+      "work breakdown structure (WBS)"
+    ],
+    note: "Source: PROEA 601 module 5 slide 6."
+  },
+  {
+    numb: "601m5p13c",
+    question: "Identify the correct process: person or group whose expectations and needs must be met",
+    answer: "stakeholder registry",
+    options: [
+      "project charter",
+      "stakeholder registry",
+      "risk registry",
+      "work breakdown structure (WBS)"
+    ],
+    note: "Source: PROEA 601 module 5 slide 13."
+  },
+  {
+    numb: "601m5p6d",
+    question: "Identify the correct process: may include project description, scope, business case, constraints, deliverables, stakeholders, issues, and approvals",
+    answer: "project charter",
+    options: [
+      "project charter",
+      "stakeholder registry",
+      "risk registry",
+      "work breakdown structure (WBS)"
+    ],
+    note: "Source: PROEA 601 module 5 slide 6."
+  },
+  {
+    numb: "601m5p16",
+    question: "Identify the correct process: proactively manage expectations",
+    answer: "stakeholder registry",
+    options: [
+      "project charter",
+      "stakeholder registry",
+      "risk registry",
+      "work breakdown structure (WBS)"
+    ],
+    note: "Source: PROEA 601 module 5 slide 16."
+  },
+  {
+    numb: "601m5p13a",
+    question: "Identify the correct process: person or group that has a business interest in the outcome of a project",
+    answer: "stakeholder registry",
+    options: [
+      "project charter",
+      "stakeholder registry",
+      "risk registry",
+      "work breakdown structure (WBS)"
+    ],
+    note: "Source: PROEA 601 module 5 slide 13."
+  },
+  {
+    numb: "601m5p13b",
+    question: "Identify the correct process: person or group that exerts influence on the objectives of a project",
+    answer: "stakeholder registry",
+    options: [
+      "project charter",
+      "stakeholder registry",
+      "risk registry",
+      "work breakdown structure (WBS)"
+    ],
+    note: "Source: PROEA 601 module 5 slide 13."
+  },
+  {
+    numb: "601m5p14",
+    question: "Identify the correct process: customers, end-users, investors/funders, management and ownership, partner groups",
+    answer: "stakeholder registry",
+    options: [
+      "project charter",
+      "stakeholder registry",
+      "risk registry",
+      "work breakdown structure (WBS)"
+    ],
+    note: "Source: PROEA 601 module 5 slide 14."
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ],
+  //  note: "Source: Made up question."
+  // },
+];
