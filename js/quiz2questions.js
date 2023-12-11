14f90ed2ed32aa474e39cdd89ec2a42b84c08c3c
csquared123 <cindy.cario@gmail.com>
2023-12-10 19:30:08 -0700

    Add files via upload

---
+++ js/quiz2questions.js
@@ -0,0 +1,138 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: "1.1",
+    question: "Why did the computer sneeze?",
+    answer: "Because it got a virus",
+    options: [
+      "404 error",
+      "Because it got a virus",
+      "Unknown",
+      "Blue screen of death"
+    ],
+    note: "Source: Made up question."
+  },
+    {
+    numb: "1.2",
+    question: "How are elephants and computers similar?",
+    answer: "They both have large memories",
+    options: [
+      "They both have large memories",
+      "huh?",
+      "I don't get it",
+      "This is not a great joke"
+    ],
+    note: "Source: Made up question."
+  },
+    {
+    numb: "1.3",
+    question: "Why did the cat sit on the computer?",
+    answer: "To keep an eye on the mouse",
+    options: [
+      "Porcupine",
+      "Zebra",
+      "Tiger",
+      "To keep an eye on the mouse"
+    ],
+    note: "Source: Made up question."
+  },
+    {
+    numb: "1.4",
+    question: "What's an alien's favorite key?",
+    answer: "The space bar",
+    options: [
+      "The space bar",
+      "The esc key",
+      "The exclamation mark",
+      "The &"
+    ],
+    note: "Source: Made up question."
+  },
+    {
+    numb: "1.5",
+    question: "How do trees use computers?",
+    answer: "They log in",
+    options: [
+      "Fir tree",
+      "They log in",
+      "Pine tree",
+      "Spruce tree"
+    ],
+    note: "Source: Made up question."
+  },
+  {
+  numb: "1.6",
+  question: "What does a website and Cookie Monster have in common?",
+  answer: "they both love cookies",
+  options: [
+    "Elmo is their friend",
+    "PBS",
+    "something about Sesame Street?",
+    "they both love cookies"
+  ],
+  note: "Source: Made up question."
+},
+{
+    numb: "1.7",
+    question: "What do you call a computer that sings?",
+    answer: "A-dell",
+    options: [
+      "Beyonce",
+      "Cher",
+      "A-dell",
+      "Aretha Franklin"
+    ],
+    note: "Source: Made up question."
+},
+{
+    numb: "1.8",
+    question: "Why did the PPT presentation cross the road?",
+    answer: "to get to the other slide",
+    options: [
+      "to get to the other slide",
+      "it was jaywalking",
+      "can you see my slides?",
+      "you're muted"
+    ],
+    note: "Source: Made up question."
+},
+{
+    numb: "1.9",
+    question: "What does a computer eat?",
+    answer: "Micro chips, one byte at a time",
+    options: [
+      "students",
+      "my homework",
+      "my unsaved files",
+      "Micro chips, one byte at a time"
+    ],
+    note: "Source: Made up question."
+},
+{
+    numb: "1.10",
+    question: "What do you call a computer superhero?",
+    answer: "A screen saver",
+    options: [
+      "A screen saver",
+      "Batman",
+      "Superman",
+      "Captain America"
+    ],
+    note: "Source: Made up question."
+},
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ],
+  //  note: "Source: Made up question."
+  // },
+];
